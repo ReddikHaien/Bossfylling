c66feed418a09072710e7d16375ca6209cd47985
ReddikHaien <fredflug@online.no>
2020-08-11 13:28:44 +0200

    test2

--- Velkommen/src/no/hvl/dat100/Velkommen.java
+++ Velkommen/src/no/hvl/dat100/Velkommen.java
@@ -5,7 +5,8 @@ public class Velkommen {
 	public static void main(String[] args) {
 		System.out.println("**********");
 		System.out.println("Nå virker det!");
-		System.out.println("**********");
+		System.out.println("**********"); 
+		
 	}
 
 }
